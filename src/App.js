d62fc86b021ac0c48196c8839fae51822e11cb09
Muhammed Beraat Kildaci <muhammedberaatkildaci@gmail.com>
2025-01-13 23:07:14 +0300

    styled components and ui framework guides

--- src/App.js
+++ src/App.js
@@ -1,19 +1,29 @@
+import { Title } from "./Components";
+import styles from "./App.module.css";
+import { Button } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+
 function App() {
   return (
-    <>
-    <div>{process.env.NODE_ENV}</div>
-    <p>{process.env.REACT_APP_API_URL}</p>
-    <p>{console.log(process.env.REACT_APP_API_URL)}</p>
-       hello world
-
-       {
-        process.env.NODE_ENV === 'production' && (
-          <>
-            <div>Merhaba dünya</div>
-          </>
-        )
-       }
-    </>
+    <div className={styles.App}>
+      <Title>{process.env.NODE_ENV}</Title>
+      <Title className="text-3xl text-red-400">{process.env.NODE_ENV}</Title>
+      <Title theme="dark">{process.env.NODE_ENV}</Title>
+      <Button as="a" variant="primary">
+        Button as link
+      </Button>
+      <Button as="a" variant="success">
+        Button as link
+      </Button>
+      <p>{process.env.REACT_APP_API_URL}</p>
+      <p>{console.log(process.env.REACT_APP_API_URL)}</p>
+      hello world
+      {process.env.NODE_ENV === "production" && (
+        <>
+          <div>Merhaba dünya</div>
+        </>
+      )}
+    </div>
   );
 }
 
