a619d8756a301127dbbfb6f2a6b32f71116a96c2
Muhammed Beraat Kildaci <muhammedberaatkildaci@gmail.com>
2025-01-13 18:32:50 +0300

    development and production enviroment variables

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
 function App() {
   return (
     <>
-      hello world
+    <div>{process.env.NODE_ENV}</div>
+    <p>{process.env.REACT_APP_API_URL}</p>
+    <p>{console.log(process.env.REACT_APP_API_URL)}</p>
+       hello world
+
+       {
+        process.env.NODE_ENV === 'production' && (
+          <>
+            <div>Merhaba dünya</div>
+          </>
+        )
+       }
     </>
   );
 }
