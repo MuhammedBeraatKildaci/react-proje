3acb31a73e20b6f5286ebac47565ab2f01f12b10
Muhammed Beraat Kildaci <muhammedberaatkildaci@gmail.com>
2025-01-14 21:11:38 +0300

    component-props-useState

--- src/App.js
+++ src/App.js
@@ -1,6 +1,26 @@
-import React, { createElement, Fragment } from "react";
+import React, { createElement, Fragment,useState} from "react";
+import Button from './components/Button';
+import Tab from "./components/Tab"
+
+
+
+
+function Profile() {
+	console.log('profil tabına geldin!')
+	return (
+		<div>
+			burası profile tabı
+		</div>
+	)
+}
+
 
 const App = () => {
+
+  const todos = ['todo1', 'todo2', 'todo3']
+
+	const [activeTab, setActiveTab] = useState(1)
+
   /**createElement
    * return createElement('main',{
       className: 'test-app',
@@ -11,8 +31,39 @@ const App = () => {
   return (
     //fragment
     <>
+        <div style={{padding: 20}}>
+				<button onClick={() => setActiveTab(2)}>
+					Aktif Tabı Değiştir
+				</button>
+				<Tab activeTab={activeTab} onChange={tabIndex => setActiveTab(tabIndex)}>
+					<Tab.Panel title="Profil"><Profile /></Tab.Panel>
+					<Tab.Panel title="Hakkında">2. tab</Tab.Panel>
+					<Tab.Panel title="Ayarlar">3. tab</Tab.Panel>
+				</Tab>
+				{activeTab === 2 && (
+					<div>
+						burası da eksta bir alan!
+					</div>
+				)}
+			</div>
+
+
       <main id="main" className="test-app">
-        hello world
+        
+      <div style={{padding: 20}}>
+				<Button>
+					Buton Örneği
+				</Button>
+				<Button variant="success">
+					Buton Örneği
+				</Button>
+				<Button variant="danger">
+					Buton Örneği
+				</Button>
+				<Button variant="warning">
+					Buton Örneği
+				</Button>
+			</div>
       </main>
     </>
   );
